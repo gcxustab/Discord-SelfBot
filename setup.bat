fc086380d753887699beb57cea7df4f379094b71
gcxustab <aidenanwar610@gmail.com>
2025-03-19 13:52:59 +0200

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,3 @@
-python -m pip install -r requirements.txt
-cls
-echo python main.py >> start.bat
-start start.bat
-start /b "" cmd /c &exit /b
+$ git clone https://github.com/AstraaDev/Discord-SelfBot.git
+$ python -m pip install -r requirements.txt
+$ python main.py
